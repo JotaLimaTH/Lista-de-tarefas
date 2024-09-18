252d82fc12d46fce1aba38ecfe1d2607a8d05a0a
JotaLimaTH <123770747+JotaLimaTH@users.noreply.github.com>
2024-09-18 19:00:13 -0300

    Lista de tarefas criada com HTML, CSS e Javascript. O código possui três funções: addTask, getTasks e deleteTask. O código vai trabalhar na DOM, criando e deletando elementos, e vai trabalhar na Local Storage.

---
+++ script.js
@@ -0,0 +1,51 @@
+getTasks();
+
+function addTask() {
+    const inputTask = document.getElementById("newTask");
+    const newTask = inputTask.value;
+    if (newTask.trim() == ""){
+        return;
+    }
+    localStorage.setItem('task', newTask);
+    const taskList = JSON.parse(localStorage.getItem('taskList')) || [];
+    taskList.push({ task: newTask, finished: 'false' })
+    localStorage.setItem('taskList', JSON.stringify(taskList));
+    inputTask.value = "";
+    getTasks()
+
+    console.log(newTask);
+    
+}
+
+function getTasks() {
+    const taskListUl = document.getElementById("taskList");
+    taskListUl.innerHTML = '';
+
+    const taskList = JSON.parse(localStorage.getItem('taskList')) || [];
+
+    taskList.forEach((task, index) => {
+        const li = document.createElement('li');
+
+        const span = document.createElement('span');
+        span.innerHTML = task.task;
+
+        const buttonDelete = document.createElement('button');
+        buttonDelete.textContent = 'Excluir';
+        buttonDelete.addEventListener('click', () => deleteTask(index));
+
+        li.appendChild(span);
+        li.appendChild(buttonDelete);
+
+        taskListUl.appendChild(li);
+
+        localStorage.removeItem("task");
+    });
+}
+
+function deleteTask(index) {
+    const taskList = JSON.parse(localStorage.getItem('taskList')) || [];
+    taskList.splice(index, 1);
+    localStorage.setItem('taskList', JSON.stringify(taskList));
+
+    getTasks();
+}
